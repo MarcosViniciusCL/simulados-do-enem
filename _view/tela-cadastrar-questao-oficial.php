947e77bf4bf8f861ed392edd3cdaa036753187e4
ecomp1997 <emanuel.santana1514@gmail.com>
2018-02-03 13:44:27 -0300

    Correções da tela de cadastrar questão oficial.
    As mudanças solicitadas que eram: a adição do ano, mudança de list para radio a opção da área de conhecimento

--- _view/tela-cadastrar-questao-oficial.php
+++ _view/tela-cadastrar-questao-oficial.php
@@ -32,33 +32,140 @@
 	include('navbar-adm.php');
 	?>	
 
-	<div id="botoes" style="margin-top:-20px" class="container-fluid card">			  
-		<div style="margin-top:10px" class="row"> 
+	<div style="background-color:#606c76" id="cadQuestao" style="margin-top:-20px" class="container-fluid card">			  
+	 	<div style="margin-top:10px" class="row">           
+            <div class=" card col-sm-12 col-md-12 col-lg-12" >
 
-			<div class="col-sm-0 col-md-3 col-lg-7">				
-			
-			</div>   
+				<div class=" card col-sm-12 col-md-8 col-lg-8">
 
-			<div class="col-sm-12 col-md-7 col-lg-7">														
-				<!-- 			
-				<button href="#" onclick="mostraOculta('cadProva','cadQuestao')" >Cadastrar Prova</button>
-				<button href="#" onclick="mostraOculta('cadQuestao','cadProva')" >Cadastrar Questão</button>
-					-->	  
-			</div> 
-			
-			<div class="col-sm-0 col-md-2 col-lg-7">
+					<div class="col-sm-12 col-md-11 col-lg-11">									
+						<form>
+							<textarea class="form-control" id="comments" style="margin-top: 10px" name="Enunciado" id="Enunciado" placeholder="Digite aqui o texto da questão." rows="7"></textarea>
+							<h4>Se houver imagem no enunciado da questão, selecione-a usando o botão abaixo:</h4>
+							<input type="file" accept=".jpeg, .png" accept=".png"  class="form-control-file" id="exampleFormControlFile1">
+							<h4>Escreva o texto das alternativas da questão, selecione a alternativa cujo a resposta é a resposta correta da questão! Caso a resposta seja uma imagem, escolha a imagem e selecione a correta! </h4>
+							<div class="col-md-7 col-lg-7 col-sm-12">
+								<div class="input-group">																		
+									<span class="input-group-addon">
+										A.<input name="correta" type="radio" aria-label="...">
+									</span>
+									<input type="text" class="form-control" aria-label="...">
+								</div><!-- /input-group -->
+
+								<div class="input-group">
+									<span class="input-group-addon">
+										B.<input name="correta" type="radio" aria-label="...">
+									</span>
+									<input type="text" class="form-control" aria-label="...">
+								</div><!-- /input-group -->
+
+								<div class="input-group">
+									<span class="input-group-addon">
+										C.<input name="correta" type="radio" aria-label="...">
+									</span>
+									<input type="text" class="form-control" aria-label="...">
+								</div><!-- /input-group -->
+
+								<div class="input-group">
+									<span class="input-group-addon">
+										D.<input name="correta" type="radio" aria-label="...">
+									</span>
+									<input type="text" class="form-control" aria-label="...">
+								</div><!-- /input-group -->
+
+								<div class="input-group">
+									<span class="input-group-addon">
+										E.<input name="correta" type="radio" aria-label="...">
+									</span>
+									<input type="text" class="form-control" aria-label="...">
+								</div><!-- /input-group -->	
+							</div>
+							<div class="col-md1 col-lg-1">
+								<div class="input-group">
+									A.<input name="imgcorreta" type="radio"><br><br>
+								</div>	
+								<div class="input-group">
+									B.<input name="imgcorreta" type="radio"><br><br>
+								</div>
+								<div class="input-group">
+									C.<input name="imgcorreta" type="radio"><br><br>
+								</div>
+								<div class="input-group">
+									D.<input name="imgcorreta" type="radio"><br><br>
+								</div>
+								<div class="input-group">
+									E.<input name="imgcorreta" type="radio"><br>
+								</div>				
+							</div>
+							<div class="col-md-4 col-lg-4 col-sm-12">
+								<div class="input-group">								
+									<input name="alternativa a" type="file" accept=".png , .jpeg">			
+								</div><!-- /input-group -->	
+								<br>
+								<div class="input-group">								
+									<input name="alternativa b" type="file" accept=".png , .jpeg">				
+								</div><!-- /input-group -->	
+								<br>
+								<div class="input-group">								
+									<input name="alternativa c" type="file" accept=".png , .jpeg">				
+								</div><!-- /input-group -->	
+								<br>
+								<div class="input-group">								
+									<input name="alternativa d" type="file" accept=".png , .jpeg">				
+								</div><!-- /input-group -->	
+								<br>
+								<div class="input-group">								
+									<input name="alternativa e" type="file" accept=".png , .jpeg">				
+								</div><!-- /input-group -->	
+									
+							</div>					
+						</form>											
+					</div>
+					<div class="col-sm-12 col-md-1 col-lg-1">					
+					</div>
+
+				</div>
 				
-			</div>
+				
 
-		</div>	
+				<div class="card col-sm-12 col-md-4 col-lg-4">
+					<form>
+						<div class="form-group">
+						
+							<h4>Escolha a área de conhecimento da questão</h4>
+							
+							<div class="input-group">
+								<input name="areaconhecimento" type="radio"> Ciências Humanas <br><br>
+							</div>
+							<div class="input-group">
+								<input name="areaconhecimento" type="radio"> Ciências da Natureza <br><br>
+							</div>
+							<div class="input-group">
+								<input name="areaconhecimento" type="radio"> Linguagens e Suas Tecnologias <br><br>
+							</div>
+							<div class="input-group">
+								<input name="areaconhecimento" type="radio"> Matemática e Suas Tecnologias <br><br>
+							</div>							
+
+							<h4>Selecione o Ano da Questão Que Está Cadastrando</h4>
+							<input type="number" name="points" min="1995" max="2017" step="1" required value="2017">							
+														
+							<br><br><br><br><br><br><br><br>			
+							<button class="button">Enviar Questão</button>
+							<button class="button">Cancelar Envio</button>	
+						</div>
+					</form>
+				</div>				            				                
+            </div>
+		</div>
 	</div>
 
-	<div  id="cadProva" style="margin-top:-20px" class="container-fluid card">			  
+	<div style="background-color:#606c76" id="cadProva" style="margin-top:-20px" class="container-fluid card">			  
 	 	<div style="margin-top:10px" class="row">  
 			<div class="col-sm-0 col-md-3 col-lg-7">
 
 			</div>   
-			<div class="col-sm-12 col-md-7 col-lg-7">
+			<div class="col-sm-12 col-md-7 col-lg-7 card">
 				<form>
 					<div class="form-group">
 						<h4>Escolha uma prova para submeter</h4><br>						
@@ -76,135 +183,6 @@
 			<div class="col-sm-0 col-md-2 col-lg-7">
 
 			</div>       
-		</div>
-	</div>
-
-	<div id="cadQuestao" style="margin-top:-20px" class="container-fluid card">			  
-	 	<div style="margin-top:10px" class="row">           
-            <div class=" card col-sm-12 col-md-12 col-lg-12">
-				<div class="col-sm-12 col-md-7 col-lg-7">									
-					<form>
-						<textarea class="form-control" id="comments" style="margin-top: 10px" name="Enunciado" id="Enunciado" placeholder="Digite aqui o texto da questão." rows="7"></textarea>
-						<h4>Se houver imagem no enunciado da questão, selecione-a usando o botão abaixo:</h4>
-						<input type="file" accept=".jpeg, .png" accept=".png"  class="form-control-file" id="exampleFormControlFile1">
-						<h4>Escreva o texto das alternativas da questão, selecione a alternativa cujo a resposta é a resposta correta da questão! Caso a resposta seja uma imagem, escolha a imagem e selecione a correta! </h4>
-						<div class="col-md-7 col-lg-7 col-sm-12">
-							<div class="input-group">																		
-								<span class="input-group-addon">
-									A.<input name="correta" type="radio" aria-label="...">
-								</span>
-								<input type="text" class="form-control" aria-label="...">
-							</div><!-- /input-group -->
-
-							<div class="input-group">
-								<span class="input-group-addon">
-									B.<input name="correta" type="radio" aria-label="...">
-								</span>
-								<input type="text" class="form-control" aria-label="...">
-							</div><!-- /input-group -->
-
-							<div class="input-group">
-								<span class="input-group-addon">
-									C.<input name="correta" type="radio" aria-label="...">
-								</span>
-								<input type="text" class="form-control" aria-label="...">
-							</div><!-- /input-group -->
-
-							<div class="input-group">
-								<span class="input-group-addon">
-									D.<input name="correta" type="radio" aria-label="...">
-								</span>
-								<input type="text" class="form-control" aria-label="...">
-							</div><!-- /input-group -->
-
-							<div class="input-group">
-								<span class="input-group-addon">
-									E.<input name="correta" type="radio" aria-label="...">
-								</span>
-								<input type="text" class="form-control" aria-label="...">
-							</div><!-- /input-group -->	
-						</div>
-						<div class="col-md1 col-lg-1">
-							<div class="input-group">
-								A.<input name="imgcorreta" type="radio"><br><br>
-							</div>	
-							<div class="input-group">
-								B.<input name="imgcorreta" type="radio"><br><br>
-							</div>
-							<div class="input-group">
-								C.<input name="imgcorreta" type="radio"><br><br>
-							</div>
-							<div class="input-group">
-								D.<input name="imgcorreta" type="radio"><br><br>
-							</div>
-							<div class="input-group">
-								E.<input name="imgcorreta" type="radio"><br>
-							</div>				
-						</div>
-						<div class="col-md-4 col-lg-4 col-sm-12">
-							<div class="input-group">								
-								<input name="alternativa a" type="file" accept=".png , .jpeg">			
-							</div><!-- /input-group -->	
-							<br>
-							<div class="input-group">								
-								<input name="alternativa b" type="file" accept=".png , .jpeg">				
-							</div><!-- /input-group -->	
-							<br>
-							<div class="input-group">								
-								<input name="alternativa c" type="file" accept=".png , .jpeg">				
-							</div><!-- /input-group -->	
-							<br>
-							<div class="input-group">								
-								<input name="alternativa d" type="file" accept=".png , .jpeg">				
-							</div><!-- /input-group -->	
-							<br>
-							<div class="input-group">								
-								<input name="alternativa e" type="file" accept=".png , .jpeg">				
-							</div><!-- /input-group -->	
-								
-						</div>					
-					</form>											
-				</div>
-				
-				<div class="col-sm-12 col-md-1 col-lg-1">					
-				</div>
-
-				<div class="col-sm-12 col-md-4 col-lg-4">
-					<form>
-						<div class="form-group">
-						
-							<h4>Escolha a área de conhecimento da questão</h4>
-							<ul class="nav navbar-nav navbar-left">
-
-								<li class="dropdown">
-									<a class="dropdown-toggle" data-toggle="dropdown" href="#">Área de conhecimento
-										<span class="caret"></span>
-									</a>
-									<ul class="dropdown-menu">
-										<li>
-											<a href="#">Ciências Humanas</a>
-										</li>
-										<li>
-											<a href="#">Ciências da Natureza</a>
-										</li>                                                     
-										
-										<li>
-											<a href="#">Matemática</a>
-										</li>
-										<li>
-											<a href="#">Linguagens</a>
-										</li>
-									</ul>
-								</li>
-							</ul>
-														
-							<br><br><br><br><br><br><br><br><br><br><br><br><br>			
-							<button class="button">Enviar Questão</button>
-							<button class="button">Cancelar Envio</button>	
-						</div>
-					</form>
-				</div>				            				                
-            </div>
 		</div>
 	</div>
 
